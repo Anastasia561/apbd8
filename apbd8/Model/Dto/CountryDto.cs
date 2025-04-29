c160f7cb7f307df9da2467028f4a71ad6f0ba1a3
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2025-04-29 14:18:47 +0200

    model layer created

---
+++ apbd8/Model/Dto/CountryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace apbd8.Model.Dto;
+
+public class CountryDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ apbd8/Model/Trip.cs
@@ -0,0 +1,12 @@
+﻿namespace apbd8.Model;
+
+public class Trip
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Destination { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int MaxPeople { get; set; }
+    public List<Country> Countries { get; set; }
+}
